2d92d1214379aef8ce5fb03cb33c0851721323aa
igor <74genesis@gmail.com>
2023-12-07 14:29:36 +0400

    add ui

---
+++ resources/js/Components/forms/SearchForm.vue
@@ -0,0 +1,116 @@
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import type { FormProps } from "element-plus";
+import WhiteBlock from "../base/WhiteBlock.vue";
+
+const fields = reactive({
+    name: "",
+    bedrooms: null,
+    bathrooms: null,
+    storeys: null,
+    garages: null,
+    priceFrom: null,
+    priceTo: null,
+});
+</script>
+
+<template>
+    <WhiteBlock>
+        <el-form
+            label-position="top"
+            label-width="100px"
+            :model="fields"
+            size="large"
+        >
+            <div class="flex gap-6">
+                <el-form-item label="Name">
+                    <el-input v-model="fields.name" clearable />
+                </el-form-item>
+                <el-form-item label="Bedrooms">
+                    <el-input-number
+                        v-model="fields.bedrooms"
+                        :min="1"
+                        :max="10"
+                        controls-position="right"
+                    />
+                </el-form-item>
+                <el-form-item label="Bathrooms">
+                    <el-input-number
+                        v-model="fields.bathrooms"
+                        :min="1"
+                        :max="10"
+                        controls-position="right"
+                    />
+                </el-form-item>
+                <el-form-item label="Storeys">
+                    <el-input-number
+                        v-model="fields.storeys"
+                        :min="1"
+                        :max="10"
+                        controls-position="right"
+                    />
+                </el-form-item>
+                <el-form-item label="Garages">
+                    <el-input-number
+                        v-model="fields.garages"
+                        :min="1"
+                        :max="10"
+                        controls-position="right"
+                    />
+                </el-form-item>
+            </div>
+            <div class="flex gap-6 justify-center">
+                <el-form-item
+                    label="Price From"
+                    :rules="[
+                        { type: 'number', message: 'price must be a number' },
+                    ]"
+                    prop="priceFrom"
+                >
+                    <el-input
+                        v-model.number="fields.priceFrom"
+                        clearable
+                        type="text"
+                        autocomplete="off"
+                        :parser="(value) => value.replace(/\$\s?|(,*)/g, '')"
+                        :formatter="
+                            (value) =>
+                                `$ ${value}`.replace(
+                                    /\B(?=(\d{3})+(?!\d))/g,
+                                    ','
+                                )
+                        "
+                    />
+                </el-form-item>
+                <el-form-item
+                    label="Price To"
+                    prop="priceTo"
+                    :rules="[
+                        { type: 'number', message: 'price must be a number' },
+                    ]"
+                >
+                    <el-input
+                        v-model.number="fields.priceTo"
+                        clearable
+                        type="text"
+                        autocomplete="off"
+                        :parser="(value) => value.replace(/\$\s?|(,*)/g, '')"
+                        :formatter="
+                            (value) =>
+                                `$ ${value}`.replace(
+                                    /\B(?=(\d{3})+(?!\d))/g,
+                                    ','
+                                )
+                        "
+                    />
+                </el-form-item>
+            </div>
+        </el-form>
+    </WhiteBlock>
+</template>
+
+<style>
+.el-input-number {
+    width: 130px;
+}
+</style>
---
+++ resources/js/Components/homepage/UsersTable.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import WhiteBlock from "../base/WhiteBlock.vue";
+
+interface User {
+    name: string;
+    price: string;
+    bedrooms: string;
+    bathrooms: string;
+    storeys: string;
+    garages: string;
+}
+
+const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
+
+const tableData: User[] = [
+    {
+        name: "User name",
+        price: "823423",
+        bedrooms: "1",
+        bathrooms: "3",
+        storeys: "5",
+        garages: "4",
+    },
+    {
+        name: "User name",
+        price: "823423",
+        bedrooms: "1",
+        bathrooms: "3",
+        storeys: "5",
+        garages: "4",
+    },
+    {
+        name: "User name",
+        price: "823423",
+        bedrooms: "1",
+        bathrooms: "3",
+        storeys: "5",
+        garages: "4",
+    },
+    {
+        name: "User name",
+        price: "823423",
+        bedrooms: "1",
+        bathrooms: "3",
+        storeys: "5",
+        garages: "4",
+    },
+];
+
+const isEmpty = ref(() => true);
+</script>
+
+<template>
+    <WhiteBlock v-loading="false">
+        <div class="flex justify-center w-full" v-if="isEmpty">
+            <el-empty description="No entries found 😭" />
+        </div>
+        <el-table :data="tableData" style="width: 100%" v-else>
+            <el-table-column label="Name" prop="name"></el-table-column>
+            <el-table-column label="Price" prop="price">
+                <template #default="scope">
+                    {{ formatter.format(scope.row.price) }}
+                </template>
+            </el-table-column>
+            <el-table-column label="Bedrooms" prop="bedrooms"></el-table-column>
+            <el-table-column
+                label="Bathrooms"
+                prop="bathrooms"
+            ></el-table-column>
+            <el-table-column label="Storeys" prop="storeys"></el-table-column>
+            <el-table-column label="Garages" prop="garages"></el-table-column>
+        </el-table>
+    </WhiteBlock>
+</template>
+
+<style></style>
