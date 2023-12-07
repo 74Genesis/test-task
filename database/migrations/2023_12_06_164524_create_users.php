f7be3b2d5b6de89eb9705bf56ecdca96933f5cbc
igor <74genesis@gmail.com>
2023-12-07 04:14:27 +0400

    add seeder

--- database/migrations/2023_12_06_164524_create_users.php
+++ database/migrations/2023_12_06_164524_create_users.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -21,8 +20,6 @@ return new class extends Migration
             $table->smallInteger('storeys');
             $table->tinyInteger('garages');
         });
-
-        $this->importCsv();
     }
 
     /**
@@ -31,26 +28,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-    }
-
-    private function importCsv() {
-        if (($open = fopen(dirname(__FILE__) . "/data/property-data.csv", "r")) !== false) {
-            $i = 0;
-            while (($data = fgetcsv($open, 1000, ",")) !== false) {
-                if (!$i++) continue;
-                DB::table('users')->insert(
-                    [
-                        'name' => $data[0],
-                        'price' => floatval($data[1]),
-                        'bedrooms' => intval($data[2]),
-                        'bathrooms' => intval($data[3]),
-                        'storeys' => intval($data[4]),
-                        'garages' => intval($data[5]),
-                    ]
-                );
-            }
-        
-            fclose($open);
-        }
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (($open = fopen(dirname(__FILE__) . "/data/property-data.csv", "r")) !== false) {
+            $i = 0;
+            while (($data = fgetcsv($open, 1000, ",")) !== false) {
+                if (!$i++) continue;
+                DB::table('users')->insert(
+                    [
+                        'name' => $data[0],
+                        'price' => floatval($data[1]),
+                        'bedrooms' => intval($data[2]),
+                        'bathrooms' => intval($data[3]),
+                        'storeys' => intval($data[4]),
+                        'garages' => intval($data[5]),
+                    ]
+                );
+            }
+        
+            fclose($open);
+        }
     }
 }
